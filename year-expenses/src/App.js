4beb0f3bed4277cb18bc905659c9f3a16517ed5d
Marcelo Goto <gotomarcelo98@gmail.com>
2022-12-26 17:46:29 -0300

    feat: :sparkles: Adding new component

--- year-expenses/src/App.js
+++ year-expenses/src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <div>
-    <h1>oi</h1>
-  </div>;
+  return (
+    <div>
+      <h1>oi</h1>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
---
+++ year-expenses/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem() {
+  return <div>
+    <h1>March 28th 2021</h1>
+    <h2>Car Insurance</h2>
+    <p>$294.67</p>
+  </div>;
+}
+
+export default ExpenseItem;
